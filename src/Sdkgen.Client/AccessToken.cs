83a6deac658300bd0b47ddd93e485cd569e3f514
Christoph Kappestein <christoph.kappestein@gmail.com>
2023-10-14 23:58:35 +0200

    update docs

--- src/Sdkgen.Client/AccessToken.cs
+++ src/Sdkgen.Client/AccessToken.cs
@@ -1,4 +1,14 @@
-﻿namespace Sdkgen.Client;
+﻿/*
+ * SDKgen is a powerful code generator to automatically build client SDKs for your REST API.
+ * For the current version and information visit <https://sdkgen.app>
+ *
+ * Copyright (c) Christoph Kappestein <christoph.kappestein@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Sdkgen.Client;
 
 public class AccessToken
 {
--- src/Sdkgen.Client/AuthenticatorFactory.cs
+++ src/Sdkgen.Client/AuthenticatorFactory.cs
@@ -1,3 +1,13 @@
+/*
+ * SDKgen is a powerful code generator to automatically build client SDKs for your REST API.
+ * For the current version and information visit <https://sdkgen.app>
+ *
+ * Copyright (c) Christoph Kappestein <christoph.kappestein@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 using Sdkgen.Client.Authenticator;
 using Sdkgen.Client.Credentials;
 using Sdkgen.Client.Exception.Authenticator;
